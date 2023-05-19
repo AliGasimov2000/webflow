74aa6e71051d6ce0e9eb1ea1a4cb06f01224e4ba
Ali Gasimov <gasimovali009@gmail.com>
2023-05-19 08:30:59 +0000

    web

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
 "use strict";
 $(document).ready(function () {
 
+
+
+
+
+    $('.btn').click(function (e) { 
+        e.preventDefault();
+        if(!$trim($('input').val())== " "){
+            let a = b => {
+                for (let i = 0; i<b; i++) {$('.minibox').append(`<h2>${b}</h2>`)};
+            }
+            
+            let h = p => {
+                for (let i = 0; i<b; i++) {$('.minibox').append(`<h2>${p}</h2>`)};
+            }
+        }
+    });
+
+
+
+
+
+
+
 });
