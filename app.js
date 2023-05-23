d9f4b26af36b449a316904dc24ced6135e98fa58
Ali Gasimov <gasimovali009@gmail.com>
2023-05-23 12:45:37 +0000

    web

--- app.js
+++ app.js
@@ -4,17 +4,16 @@ $(document).ready(function () {
 
 
 
-
     $('.btn').click(function (e) { 
         e.preventDefault();
-        if(!$trim($('input').val())== " "){
+        if(!$.trim($('input').val()) == ''){
             let a = b => {
-                for (let i = 0; i<b; i++) {$('.minibox').append(`<h2>${b}</h2>`)};
+                for (let i = 0; i < b; i++) {$('.minibox2').append(`<h2>${b}</h2>`) };
             }           
-            let v = f => Number(f) ? a(f) : $('.minibox').append(`<h2>${f}</h2>`);
+            let v = f => Number(f) ? a(f) : $('.minibox2').append(`<h2>${f}</h2>`);
             v($('input').val());
         }
-        $('input').val(' ')
+        $('input').val('')
     });
 
 
